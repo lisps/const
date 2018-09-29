1b3ead680c19cd185a2c7625c787dcf4db9350f6
peterfromearth <coder@peterfromearth.de>
2018-09-29 17:56:07 +0200

    evalmath fix constructor

--- class.evalmath.php
+++ class.evalmath.php
@@ -99,7 +99,7 @@
 			'tan','tanh','arctan','atan','arctanh','atanh',
 			'sqrt','abs','ln','log');
 
-		function EvalMath() {
+		function __construct() {
 			// make the variables a little more accurate
 			$this->v['pi'] = pi();
 			$this->v['e'] = exp(1);
